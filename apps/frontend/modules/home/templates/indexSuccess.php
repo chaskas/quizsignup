cc0a252c23b5f1734ac3fe28bd289b36e0c4db1d
Rodrigo Campos <mr.chaskas@gmail.com>
2012-06-25 04:32:48 -0400

    asd

--- apps/frontend/modules/home/templates/indexSuccess.php
+++
@@ -1,8 +0,0 @@
-<p style="text-align: center;margin-top: 50px;margin-bottom: 50px;">
-Bienvenido,
-<br/>
-Si deseas inscribirte en el Programa English Online haz click aqu&iacute;
-<br/><br/>
-o si ya te registraste por favor <a href="<?php echo url_for("@sf_guard_signin"); ?>">inicia sesi&oacute;n</a>
-</p>
-
