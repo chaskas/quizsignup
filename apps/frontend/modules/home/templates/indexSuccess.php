c35cf1c07f9f7a739b07121fa0d039a94b1ab1bf
Rodrigo Campos <mr.chaskas@gmail.com>
2012-06-25 03:52:09 -0400

    trabajando en preinscrpition

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -5,3 +5,4 @@ Si deseas inscribirte en el Programa English Online haz click aqu&iacute;
 <br/><br/>
 o si ya te registraste por favor <a href="<?php echo url_for("@sf_guard_signin"); ?>">inicia sesi&oacute;n</a>
 </p>
+
